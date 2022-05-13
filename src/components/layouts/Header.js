c7649432d0dc6e7373391b70ffcc75eec3faf630
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-05-13 19:54:45 +0100

    up

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -73,7 +73,7 @@ const Header = () => {
 
                         </div>
 
-                    ) : ( !loading && <Link to="/login" className="btn ml-4" id="login_btn">Login</Link> )
+                    ) : ( !user && <Link to="/login" className="btn ml-4" id="login_btn">Login</Link> )
                     } 
 
 
